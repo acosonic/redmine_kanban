a5bc11dd0391c241608548f9a79076f07dd76676
Aleksandar Pavic <acosonic@Aleksandars-MacBook-Pro.local>
2015-12-08 22:22:13 +0100

    Trying to get it work with Redmine 3.x

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.resource :kanban, :member => {:sync => :put} do |kanban|
-    kanban.resources :user_kanbans, :as => 'users'
-    kanban.resource :user_kanbans, :as => 'my-requests'
-    kanban.resources :assigned_kanbans, :as => 'assigned-to'
+    kanban.resources :user_kanbans, :as => 'users', :via => [:get, :post]
+    kanban.resource :user_kanbans, :as => 'my-requests', :via => [:get, :post]
+    kanban.resources :assigned_kanbans, :as => 'assigned-to', :via => [:get, :post]
     kanban.resource :assigned_kanbans, :as => 'my-assigned', :only => [:show]
     kanban.resource :kanban_overviews, :as => 'overview', :only => [:show]
   end
