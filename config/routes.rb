c053ea7b69901771222d710a8a5760aa255af258
NRG Systems implementacija <nrgsystems@vmi36321.contabo.host>
2015-12-10 18:14:01 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,31 @@
-resources :kanban do
-  member do
-    put :sync
-    resources :user_kanbans
-    resources :assigned_kanbans
-    resources :kanban_overviews
+ RedmineApp::Application.routes.draw do
+    resource :kanban do
+      put :sync, :on => :member
+      
+      resources :user_kanbans do
+        get 'users' => 'user_kanbans#index'
+      end
+      resource :user_kanbans do
+        get 'my-requests' => 'user_kanbans#show'
+      end
+      resources :assigned_kanbans do
+        get 'assigned-to' => 'assigned_kanbans#index'
+      end
+      resource :assigned_kanbans, :only => [:show] do
+        get 'my-assigned' => 'assigned_kanbans#show'
+      end
+      resource :kanban_overviews, :only => [:show] do
+        get 'overview' => 'kanban_overviews#show'
+      end
+    end
     resources :kanban_issues
   end
-end
+#resources :kanban do
+#  member do
+#    put :sync
+#    resources :user_kanbans
+#    resources :assigned_kanbans
+#    resources :kanban_overviews
+#    resources :kanban_issues
+#  end
+#end
